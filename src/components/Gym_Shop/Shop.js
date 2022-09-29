a44d6478e2fdda2d0b59e64fbcfdfbab8c736406
Imtiaj09 <ahmimtiaj@gmail.com>
2022-09-29 20:26:14 +0600

    data has been loaded from json file

--- src/components/Gym_Shop/Shop.js
+++ src/components/Gym_Shop/Shop.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('products.json')
+      .then(res => res.json())
+      .then(data => setProducts(data))
+  }, [])
   return (
     <div className='shop-container'>
       <div className="products-container">
         <h3>Select today’s exercise</h3>
+        {
+          products.map(product => <Product
+            key={product.id}
+            product={product}
+          ></Product>)
+        }
       </div>
       <div className="cart-container">
-        <h4>Zahid Hossain</h4>
+        <h4>Imtiaj</h4>
       </div>
     </div>
   );
---
+++ src/components/Product/Product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Product = (props) => {
+  const { name, Time required, For age, img } = props.product
+  return (
+    <div>
+      <h2>This is product</h2>
+    </div>
+  );
+};
+
+export default Product;
