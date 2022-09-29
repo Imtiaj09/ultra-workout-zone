117d13ac40e4e36584c3f09e2a24347a5cba9a57
Imtiaj09 <ahmimtiaj@gmail.com>
2022-09-29 21:49:36 +0600

    dinamic card has been added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,11 +2,21 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
+  console.log(props)
   const { name, time, age, img } = props.product
+  console.log(time)
+  console.log(age)
   return (
     <div className='product'>
       <img src={img} alt="" />
-      <p className='product-name'>{name}</p>
+      <div className='product-info'>
+        <p className='product-name'>{name}</p>
+        <p>For Age: {age}</p>
+        <p>Time required:{time}</p>
+      </div>
+      <button className='btn-cart'>
+        <p>Add to list</p>
+      </button>
     </div>
   );
 };
