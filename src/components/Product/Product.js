2f841674cab124b0f12e704aa594290312ecda32
Imtiaj09 <ahmimtiaj@gmail.com>
2022-09-30 01:19:50 +0600

    data added in dinamic button has been added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,18 +3,17 @@ import './Product.css'
 
 const Product = (props) => {
   console.log(props)
-  const { name, time, age, img } = props.product
-  console.log(time)
-  console.log(age)
+  const { handleAddToCart, product } = props;
+  const { name, time, age, img } = product
   return (
     <div className='product'>
       <img src={img} alt="" />
       <div className='product-info'>
         <p className='product-name'>{name}</p>
         <p>For Age: {age}</p>
-        <p>Time required:{time}</p>
+        <p>Time required: {time}s</p>
       </div>
-      <button className='btn-cart'>
+      <button onClick={() => handleAddToCart(product)} className='btn-cart'>
         <p>Add to list</p>
       </button>
     </div>
