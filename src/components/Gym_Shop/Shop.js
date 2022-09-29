2cf314fcdca431c8cf72e254ce3a994785c013f3
Imtiaj09 <ahmimtiaj@gmail.com>
2022-09-30 01:33:38 +0600

    data has been added in cart section

--- src/components/Gym_Shop/Shop.js
+++ src/components/Gym_Shop/Shop.js
@@ -4,6 +4,7 @@ import './Shop.css'
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     fetch('products.json')
@@ -13,6 +14,8 @@ const Shop = () => {
 
   const handleAddToCart = (product) => {
     console.log(product);
+    const newCart = [...cart, product];
+    setCart(newCart)
   }
 
   return (
@@ -28,6 +31,7 @@ const Shop = () => {
       </div>
       <div className="cart-container">
         <h4>Imtiaj</h4>
+        <p>Selected Items: {cart.length}</p>
       </div>
     </div>
   );
